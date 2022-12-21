f02388ed1d1b8641e228f336f238e8ae095a462d
dadicx <franek.vojtech01@gmail.com>
2022-12-21 10:33:42 +0100

    progress_1

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+function getDetails(){
+    let firstName, lastName, emailAddress, password;
+    firstName = document.getElementById('first-name').innerText;
+    lastName = document.getElementById('last-name').innerText;
+    emailAddress = document.getElementById('email-address').innerText
+    password = document.getElementById('password').innerText;
+}
+console.log(firstName, lastName, emailAddress, password);
+
+function pushToServer(data){
+    let formData[];
+    for (let i = 0; i<data.length; i++){
+        if(data.document.getElementById('password').value) != null{
+            formData[i] = data.
+        }
+    }
+}
+
+
+function passwordCheck(pwd){
+
+}
